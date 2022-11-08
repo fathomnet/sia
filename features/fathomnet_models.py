bce9d509f4b854dfed1d85ea92e88786d12ad2d5
Genevieve Patterson <gen@trash.app>
2022-11-08 10:24:31 -0500

    added stub file for fathomnet model and feature extractor functions

---
+++ features/fathomnet_models.py
@@ -0,0 +1,11 @@
+
+'''
+This is a module where FathomNet specific feature extractors are defined. These
+feature extractors are expected to conform to the FeatureSpec object model
+requirements.
+'''
+
+from extract import ReducibleFeature
+
+class BenthicSupercategoryFeature(ReducibleFeature):
+    pass
